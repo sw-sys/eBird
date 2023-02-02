4fdf19054a44f6c4288e84835045d0f3a66dcf0e
S <sheldonkorpet@gmail.com>
2023-02-02 16:24:43 +0000

    Update manchester_bird_finder.py
    updating code to export JSON to CSV format

--- manchester_bird_finder.py
+++ manchester_bird_finder.py
@@ -1,15 +1,14 @@
 from ebird.api  import get_notable_observations
 import json
+import pandas as pd
 
 api_key = "APIKEYHERE"
 
-  # Get interesting birds seen in a specific location
+# Get interesting birds seen in a specific location
 interesting_records_manc = get_notable_observations(api_key, 'GB-ENG-MAN') # arg2 e.g. US-NY
-  # create dictionary of data
-dict = interesting_records_manc
-  # open new file
-out_file = open("manchester_interesting_records.json", "w")
-  # put json in to file
-json.dump(dict, out_file, indent=6)
-  #close file
-out_file.close()
+
+# write json and create df
+df = pd.read_json('manchester_interesting_records.json')
+
+#convert df to csv
+df.to_csv('interesting-manchester-birds.csv')
